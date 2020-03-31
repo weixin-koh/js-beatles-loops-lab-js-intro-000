5f01b1c2a1ede3349b8ba9e29fc83818000ada9a
Wei Xin Koh <e0201350@u.nus.edu>
2020-03-31 20:24:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+
+  for (let i = 0; i < musicians.length; i++) {
+    array.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+
+  return array;
+}
+
+
+function johnLennonFacts(facts) {
+  var counter = 0;
+
+  while (counter < facts.length) {
+    facts[counter] = facts[counter] + "!!!";
+    counter++;
+  }
+
+  return facts;
+}
+
+function iLoveTheBeatles(number) {
+  var array = [];
+
+  do {
+    array.push("I love the Beatles!");
+    number++;
+  } while (number < 15);
+
+  return array;
+}
